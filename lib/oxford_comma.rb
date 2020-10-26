4299b75c30de0fa6533b8e049f71cd2fac704354
user <github email address>
2020-10-26 19:36:08 -0400

    commit tree 4aca8e6b538d078a104a49332a85c7da22bb5104

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,21 @@
-def oxford_comma(array)
+require 'pry'
 
-end
+def oxford_comma(array)
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+          array.join(" and ")
+    
+      
+
+    else
+        element = array.pop
+        array.join(", ") << ", and #{element}"
+    end 
+end 
+ 
+
+
+
+ 
+
